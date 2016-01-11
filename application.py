d06dca9a267f7ced606124d4c555f8e5d4096d6a
Miguel Angulo <maangulo12@gmail.com>
2016-01-11 11:11:17 -0600

    Add application run module

---
+++ application.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+    BuildersRecords Web App
+    ~~~~~~~~~~~~~~~~~~~~~~~
+    :copyright: (c) 2016
+
+    Run this module to deploy the application.
+
+    -How to run it (type the following in the command-line):
+        python3 application.py
+"""
+
+from app import app
+
+
+if __name__ == '__main__':
+    app.run(host=app.config['SERVER_HOST'], port=app.config['SERVER_PORT'])
