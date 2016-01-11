4af621d8d517fecbc83683cf28451cf332a92e38
Miguel Angulo <maangulo12@gmail.com>
2016-01-11 11:20:48 -0600

    Add app modules

---
+++ app/static/angular/app.module.js
@@ -0,0 +1,30 @@
+(function() {
+    'use strict';
+
+    angular.module('app', [
+        'ngMessages',
+        'ui.validate',
+        'ui.router',
+        'angular-jwt',
+        'angular-storage',
+        'smart-table',
+        'app.directives',
+        'app.directive.email-availability',
+        'app.directive.username-availability',
+        'app.services',
+        'app.service.auth',
+        'app.service.stripe',
+        'app.home',
+        'app.login',
+        'app.signup',
+        'app.account',
+        'app.account.billing',
+        'app.projects',
+        'app.projects.overview',
+        'app.projects.budget',
+        'app.projects.funds',
+        'app.projects.expenditures',
+        'app.projects.subcontractors',
+        'app.tutorial'
+    ]);
+})();
