9bb14c309b306cb38664e5f9e23f837d71b869ca
Miguel Angulo <maangulo12@gmail.com>
2016-01-11 11:35:33 -0600

    Add username availability directive

---
+++ app/static/angular/directives/username-availability.directive.js
@@ -0,0 +1,36 @@
+(function() {
+    'use strict';
+
+    /**
+    * @desc checks if a username already exists
+    * @example <input username-availability></input>
+    */
+    angular
+        .module('app.directive.username-availability', [])
+        .directive('usernameAvailability', usernameAvailability);
+
+    function usernameAvailability($q, authService) {
+        var directive = {
+            restrict: 'A',
+            require:  'ngModel',
+            link:     linkFunc,
+        };
+        return directive;
+
+        function linkFunc(scope, el, attr, ctrl) {
+            ctrl.$asyncValidators.usernameAvailability = function(username) {
+                return authService.checkUsername(username)
+                    .then(responseHandler)
+                    .catch(errorHandler);
+                function responseHandler(response) {
+                    ctrl.$setValidity('usernameAvailability', true);
+                    return response;
+                }
+                function errorHandler(response) {
+                    ctrl.$setValidity('usernameAvailability', false);
+                    return $q.reject(response);
+                }
+            }
+        }
+    }
+})();
