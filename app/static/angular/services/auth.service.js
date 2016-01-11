a277fdc2db54d9af00ab627951c33807d97a2459
Miguel Angulo <maangulo12@gmail.com>
2016-01-11 11:35:53 -0600

    Add auth service

---
+++ app/static/angular/services/auth.service.js
@@ -0,0 +1,55 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app.service.auth', [])
+        .factory('authService', authService);
+
+    authService.$inject = ['$http', 'store', 'jwtHelper', '$q'];
+
+    function authService($http, store, jwtHelper, $q) {
+        var url = '/api/auth';
+        var service = {
+            authenticate:  authenticate,
+            checkEmail:    checkEmail,
+            checkUsername: checkUsername
+        };
+        return service;
+
+        // POST /api/auth (authenticates user)
+        function authenticate(login, password) {
+            var data = {
+                login:    login,
+                password: password
+            };
+            return $http.post(url, data)
+                .then(responseHandler)
+                .catch(errorHandler);
+
+                function responseHandler(response) {
+                    var token   = response.data.token;
+                    var payload = jwtHelper.decodeToken(token);
+                    var user    = {
+                        id:        payload.user_id,
+                        stripe_id: payload.stripe_id
+                    };
+                    store.set('jwt', token);
+                    store.set('user', user);
+                    return response;
+                }
+                function errorHandler(response) {
+                    return $q.reject(response);
+                }
+        }
+
+        // POST /api/auth/<email> (checks if email already exists)
+        function checkEmail(email) {
+            return $http.post(url + '/email', { email : email });
+        }
+
+        // POST /api/auth/<username> (checks if username already exists)
+        function checkUsername(username) {
+            return $http.post(url + '/username', { username : username });
+        }
+    }
+})();
