7df36f0854ef533a0aaeca58ad6089ec566018b8
Miguel Angulo <maangulo12@gmail.com>
2016-01-13 17:13:22 -0600

    Update utility service

--- app/static/angular/services/utility.service.js
+++ app/static/angular/services/utility.service.js
@@ -10,8 +10,9 @@
     function utilityService($http, store) {
         var url = store.get('api_url') + '/api/utility';
         var service = {
-            verifyEmail:    verifyEmail,
-            verifyUsername: verifyUsername
+            verifyEmail:       verifyEmail,
+            verifyUsername:    verifyUsername,
+            parseUbuilditFile: parseUbuilditFile
         };
         return service;
 
@@ -21,6 +22,25 @@
 
         function verifyUsername(username) {
             return $http.post(url + '/username', { username : username });
+        }
+
+        function parseUbuilditFile(vm, file) {
+            var data = new FormData();
+            data.append('file', file);
+            data.append('name', vm.project.name);
+            data.append('address', vm.project.address);
+            data.append('city', vm.project.city);
+            data.append('state', vm.project.state);
+            data.append('zipcode', vm.project.zipcode);
+            data.append('home_sq', vm.project.home_sq);
+            data.append('project_type', vm.project.type);
+            data.append('user_id', store.get('user').id);
+            
+            var config = {
+                transformRequest: angular.identity,
+                headers: { 'content-type': undefined }
+            };
+            return $http.post(url + '/ubuildit/parse', data, config);
         }
     }
 })();
