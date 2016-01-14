3db85b024d0c1fb783ddb8cb2387107751274525
Miguel Angulo <maangulo12@gmail.com>
2016-01-13 21:23:04 -0600

    Update user service

--- app/static/angular/services/user.service.js
+++ app/static/angular/services/user.service.js
@@ -10,20 +10,9 @@
     function userService($http, store) {
         var url = store.get('api_url') + '/api/users';
         var service = {
-            create:   create,
             retrieve: retrieve
         };
         return service;
-
-        function create(vm, stripe_id) {
-            var data = {
-                email:     vm.email,
-                username:  vm.username,
-                password:  vm.password,
-                stripe_id: stripe_id
-            };
-            return $http.post(url, data);
-        }
 
         function retrieve() {
             return $http.get(url + '/' + store.get('user').id);
