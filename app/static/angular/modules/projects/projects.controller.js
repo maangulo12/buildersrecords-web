56a33ca804f1ef853d6ce5bf144bced0fc53c17b
Miguel Angulo <maangulo12@gmail.com>
2016-01-13 20:51:10 -0600

    Update projects module

--- app/static/angular/modules/projects/projects.controller.js
+++ app/static/angular/modules/projects/projects.controller.js
@@ -5,111 +5,126 @@
         .module('app.projects')
         .controller('ProjectsController', ProjectsController);
 
-    ProjectsController.$inject = ['$scope', 'projectsService'];
+    ProjectsController.$inject = ['$scope', 'store', 'projectsService', 'utilityService'];
 
-    function ProjectsController($scope, projectsService) {
+    function ProjectsController($scope, store, projectsService, utilityService) {
         var vm = this;
         getProjects();
 
         function getProjects() {
             return projectsService.retrieveList()
-                .then(responseHandler)
-                .catch(errorHandler);
+                .then(showProjectList)
+                .catch(error);
 
-            function responseHandler(response) {
-                $scope.project_list = response.data.objects;
+            function showProjectList(response) {
+                vm.projectList = response.data.objects;
             }
-            function errorHandler(error) {
-                $scope.error_msg_get = true;
+            function error(response) {
+                vm.errorGetProjects = true;
             }
         }
 
-        // $scope.clickedProject = function(project) {
-        //     var index = $scope.project_list.indexOf(project);
-        //     if (index !== -1) {
-        //         store.set('project', project);
-        //         return true;
-        //     }
-        //     return false;
-        // }
-        //
-        // // ADD PROJECT functions
-        // $scope.showNewProjectModal = function() {
-        //     var btn = $('#create-project-btn').button('reset');
-        //     $scope.project = {};
-        //     $scope.new_project_form.$setPristine();
-        //     $('#new_project_modal').modal('show');
-        // }
-        // $scope.createProject = function() {
-        //     var btn = $('#create-project-btn').button('loading');
-        //
-        //     if ($('#project_file').length && $('#project_file')[0].files[0]) {
-        //         var form = new FormData();
-        //         form.append('file', $('#project_file')[0].files[0]);
-        //         form.append('name', $scope.project.name);
-        //         form.append('address', $scope.project.address);
-        //         form.append('city', $scope.project.city);
-        //         form.append('state', $scope.project.state);
-        //         form.append('zipcode', $scope.project.zipcode);
-        //         form.append('home_sq', $scope.project.home_sq);
-        //         form.append('project_type', $scope.project.type);
-        //         form.append('user_id', store.get('user').id);
-        //
-        //         UploadService.uploadUbuildit(form).then(function(response) {
-        //             $('#new_project_modal').modal('hide');
-        //             getProjects();
-        //         }, function(error) {
-        //             $scope.new_project_form.$invalid = true;
-        //             btn.button('reset');
-        //         });
-        //     } else {
-        //         ProjectService.addProject($scope.project).then(function(response) {
-        //             $('#new_project_modal').modal('hide');
-        //             getProjects();
-        //         }, function(error) {
-        //             $scope.new_project_form.$invalid = true;
-        //             btn.button('reset');
-        //         });
-        //     }
-        // }
-        //
-        // // DELETE PROJECT functions
-        // $scope.showDeleteProjectModal = function() {
-        //     $scope.error_msg_delete = false;
-        //     $('#delete_project_modal').modal('show');
-        // }
-        // $scope.deleteProject = function() {
-        //     var btn = $('#delete-project-btn').button('loading');
-        //     ProjectService.deleteProject().then(function(response) {
-        //         $('#delete_project_modal').modal('hide');
-        //         getProjects();
-        //     }, function(error) {
-        //         $scope.error_msg_delete = true;
-        //         btn.button('reset');
-        //     });
-        // }
-        //
-        // // UPDATE PROJECT functions
-        // $scope.showEditProjectModal = function(project) {
-        //     $scope.updated_project         = {};
-        //     $scope.updated_project.name    = store.get('project').name;
-        //     $scope.updated_project.address = store.get('project').address;
-        //     $scope.updated_project.city    = store.get('project').city;
-        //     $scope.updated_project.state   = store.get('project').state;
-        //     $scope.updated_project.zipcode = store.get('project').zipcode;
-        //     $scope.updated_project.home_sq = store.get('project').home_sq;
-        //     $scope.edit_project_form.$setPristine();
-        //     $('#edit_project_modal').modal('show');
-        // }
-        // $scope.updateProject = function() {
-        //     var btn = $('#update-project-btn').button('loading');
-        //     ProjectService.updateProject($scope.updated_project).then(function(response) {
-        //         $('#edit_project_modal').modal('hide');
-        //         getProjects();
-        //     }, function(error) {
-        //         $scope.edit_project_form.$invalid = true;
-        //         btn.button('reset');
-        //     });
-        // }
+        $scope.clickedProject = function(project) {
+            var index = vm.projectList.indexOf(project);
+            if (index !== -1) {
+                store.set('project', project);
+                return true;
+            }
+            return false;
+        }
+
+        $scope.showNewProjectModal = function() {
+            vm.project = {};
+            $scope.newProjectForm.$setPristine();
+            $('#new-project-modal').modal('show');
+        }
+        $scope.createProject = function() {
+            var btn = $('#create-project-button').button('loading');
+            var exists = $('#project-file').length;
+
+            if (exists && $('#project-file')[0].files[0]) {
+                parseFile()
+                    .then(projectSuccess)
+                    .catch(error);
+            } else {
+                createProject()
+                    .then(projectSuccess)
+                    .catch(error);
+            }
+
+            function parseFile() {
+                var file = $('#project-file')[0].files[0];
+                return utilityService.parseUbuilditFile(vm.project, file);
+            }
+            function createProject() {
+                return projectsService.create(vm.project);
+            }
+            function projectSuccess(response) {
+                $('#new-project-modal').modal('hide');
+                btn.button('reset');
+                getProjects();
+            }
+            function error(response) {
+                $scope.newProjectForm.$invalid = true;
+                btn.button('reset');
+            }
+        }
+
+        $scope.showDeleteProjectModal = function() {
+            vm.errorMsgDelete = false;
+            $('#delete-project-modal').modal('show');
+        }
+        $scope.deleteProject = function() {
+            var btn = $('#delete-project-button').button('loading');
+
+            deleteProject()
+                .then(deleteSuccess)
+                .catch(error);
+
+            function deleteProject() {
+                return projectsService.remove();
+            }
+            function deleteSuccess(response) {
+                $('#delete-project-modal').modal('hide');
+                btn.button('reset');
+                getProjects();
+            }
+            function error(response) {
+                vm.errorMsgDelete = true;
+                btn.button('reset');
+            }
+        }
+
+        $scope.showEditProjectModal = function(project) {
+            vm.updatedProject         = {};
+            vm.updatedProject.name    = store.get('project').name;
+            vm.updatedProject.address = store.get('project').address;
+            vm.updatedProject.city    = store.get('project').city;
+            vm.updatedProject.state   = store.get('project').state;
+            vm.updatedProject.zipcode = store.get('project').zipcode;
+            vm.updatedProject.home_sq = store.get('project').home_sq;
+            $scope.editProjectForm.$setPristine();
+            $('#edit-project-modal').modal('show');
+        }
+        $scope.updateProject = function() {
+            var btn = $('#update-project-button').button('loading');
+
+            updateProject()
+                .then(updateSuccess)
+                .catch(error);
+
+            function updateProject() {
+                return projectsService.update(vm.updatedProject);
+            }
+            function updateSuccess() {
+                $('#edit-project-modal').modal('hide');
+                btn.button('reset');
+                getProjects();
+            }
+            function error() {
+                $scope.editProjectForm.$invalid = true;
+                btn.button('reset');
+            }
+        }
     }
 })();
