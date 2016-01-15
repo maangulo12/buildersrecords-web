508c902beebf48a5acc9e93ee38a4dc1f5f28a04
Miguel Angulo <maangulo12@gmail.com>
2016-01-15 16:47:03 -0600

    Update fund service

--- app/static/angular/services/fund.service.js
+++ app/static/angular/services/fund.service.js
@@ -10,12 +10,38 @@
     function fundService($http, store) {
         var url = store.get('api_url') + '/api/funds';
         var service = {
-            retrieve: retrieve
+            retrieveList: retrieveList,
+            create:   create,
+            update:   update,
+            remove:   remove
         };
         return service;
 
-        function retrieve() {
+        function retrieveList() {
 			return $http.get(url + query('project_id', 'equals', store.get('project').id));
+        }
+
+        function create(vm) {
+            var data = {
+                name:       vm.name,
+                loan:       vm.loan,
+                amount:     vm.amount,
+                project_id: store.get('project').id
+            };
+            return $http.post(url, data);
+        }
+
+        function update(vm) {
+            var data = {
+                name:       vm.name,
+                amount:     vm.amount,
+                project_id: store.get('project').id
+            };
+            return $http.put(url + '/' + store.get('fund').id, data);
+        }
+
+        function remove() {
+            return $http.delete(url + '/' + store.get('fund').id);
         }
     }
 
