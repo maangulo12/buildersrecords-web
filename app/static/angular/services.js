8c5d419de9a8b708600b14fd984f419956560ca7
Miguel Angulo <maangulo12@gmail.com>
2016-01-15 16:46:55 -0600

    Update services

--- app/static/angular/services.js
+++ app/static/angular/services.js
@@ -4,30 +4,6 @@ function query(name, op, val) {
     return '?q={"filters":[{"name":"' + name + '","op":"' + op + '","val":"' + val + '"}]}';
 }
 
-/*
-    Functions for User service:
-*/
-app.service('User', function($http, store) {
-    var url_prefix = '/api/users';
-    // GET /api/users/<id>
-    this.retrieve = function() {
-        return $http.get(url_prefix + '/' + store.get('user').id);
-    }
-    // PUT /api/users/<id>
-    this.update = function(form) {
-        return $http.put(url_prefix + '/' + store.get('user').id, {
-            email:    form.email,
-            username: form.username
-        });
-    }
-    // PUT User password /api/users/<id>
-    this.updatePassword = function(password) {
-        return $http.put(url_prefix + '/' + store.get('user').id, {
-            password: password
-        });
-    }
-});
-
 // Functions for /api/categories
 app.service('CategoryService', function($http, store) {
     // API: Categories entry point
@@ -97,68 +73,6 @@ app.service('ItemService', function($http, store) {
     // DELETE BULK Items
     this.deleteBulkItems = function() {
         return $http.delete(api_entry + query('category_id', 'equals', store.get('category').id));
-    }
-});
-
-// Functions for /api/funds
-app.service('FundService', function($http, store) {
-    // API: Funds entry point
-    var api_entry = '/api/funds';
-    // GET list of Funds
-    this.getFunds = function() {
-        return $http.get(api_entry + query('project_id', 'equals', store.get('project').id));
-    }
-    // POST Fund
-    this.addFund = function(form) {
-        return $http.post(api_entry, {
-            name:       form.name,
-            loan:       form.loan,
-            amount:     form.amount,
-            project_id: store.get('project').id
-        });
-    }
-    // PUT Fund
-    this.updateFund = function(form) {
-        return $http.put(api_entry + '/' + store.get('fund').id, {
-            name:       form.name,
-            loan:       form.loan,
-            amount:     form.amount,
-            project_id: store.get('project').id
-        });
-    }
-    // DELETE Fund
-    this.deleteFund = function() {
-        return $http.delete(api_entry + '/' + store.get('fund').id);
-    }
-});
-
-// Functions for /api/draws
-app.service('DrawService', function($http, store) {
-    // API: Draws entry point
-    var api_entry = '/api/draws';
-    // POST Draw
-    this.addDraw = function(form) {
-        return $http.post(api_entry, {
-            date:    form.date,
-            amount:  form.amount,
-            fund_id: store.get('fund').id
-        });
-    }
-    // PUT Draw
-    this.updateDraw = function(form) {
-        return $http.put(api_entry + '/' + store.get('draw').id, {
-            date:    form.date,
-            amount:  form.amount,
-            fund_id: store.get('fund').id
-        });
-    }
-    // DELETE Draw
-    this.deleteDraw = function(draw_id) {
-        return $http.delete(api_entry + '/' + draw_id);
-    }
-    // DELETE BULK Draws
-    this.deleteBulkDraws = function() {
-        return $http.delete(api_entry + query('fund_id', 'equals', store.get('fund').id));
     }
 });
 
