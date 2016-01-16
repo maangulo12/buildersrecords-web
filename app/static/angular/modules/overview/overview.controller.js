253661b043e08a70540c2cd9fb81fc1b765d3730
Miguel Angulo <maangulo12@gmail.com>
2016-01-16 17:00:43 -0600

    Update overview module

--- app/static/angular/modules/overview/overview.controller.js
+++ app/static/angular/modules/overview/overview.controller.js
@@ -10,140 +10,124 @@
     function OverviewController($scope, store, categoryService) {
         var vm = this;
         vm.project = store.get('project');
-        var options = chartOptions();
-        var barchart = $('#barchart-container').highcharts(options);
-        displayChart();
-        displayTable();
 
-        function chartOptions() {
-            var opt = {
-                chart: {
-                    type: 'column'
-                },
+        getCategories()
+            .then(updateChart)
+            .then(updateTable)
+            .catch(error);
+
+        var options = {
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: ''
+            },
+            tooltip: {
+                headerFormat: '<span style="font-size:14px"> {point.key} </span><table>',
+                pointFormat:  '<tr><td style="color: {series.color}"> {series.name}: </td>' +
+                              '<td> <b> ${point.y:.2f} </b> </td></tr>',
+                footerFormat: '</table>',
+                shared:       true,
+                useHTML:      true
+            },
+            xAxis: {
+                categories: [],
+                crosshair: true
+            },
+            yAxis: {
                 title: {
-                    text: ''
-                },
-                tooltip: {
-                    headerFormat: '<span style="font-size:14px"> {point.key} </span><table>',
-                    pointFormat:  '<tr><td style="color: {series.color}"> {series.name}: </td>' +
-                                  '<td> <b> ${point.y:.2f} </b> </td></tr>',
-                    footerFormat: '</table>',
-                    shared:       true,
-                    useHTML:      true
-                },
-                xAxis: {
-                    categories: [],
-                    crosshair: true
-                },
-                yAxis: {
-                    title: {
-                        text: 'Dollars'
-                    }
-                },
-                series: [{
-                    name: 'Estimated Cost',
-                    data: []
-                }, {
-                    name: 'Actual Cost',
-                    data: []
-                }, {
-                    name: 'Paid',
-                    data: []
-                }],
-                credits: {
-                    enabled: false
+                    text: 'Dollars'
                 }
-            };
-            return opt;
+            },
+            series: [{
+                name: 'Estimated Cost',
+                data: []
+            }, {
+                name: 'Actual Cost',
+                data: []
+            }, {
+                name: 'Paid',
+                data: []
+            }],
+            credits: {
+                enabled: false
+            }
+        };
+
+        function getCategories() {
+            return categoryService.retrieveList();
         }
 
-        function displayChart() {
-            return categoryService.retrieveList()
-                .then(getSuccess)
-                .catch(error);
+        function updateChart(response) {
+            var categoryList = response.data.objects;
 
-            function getSuccess(response) {
-                options.xAxis.categories = [];
-	            options.series[0].data   = [];
-	            options.series[1].data   = [];
-	            options.series[2].data   = [];
-
-	            if (response.data.objects.length == 0) {
-	                options.xAxis.categories.push('No categories');
-	                options.series[0].data.push(0.00);
-	                options.series[1].data.push(0.00);
-	                options.series[2].data.push(0.00);
-	            } else {
-	                angular.forEach(response.data.objects, function(category) {
-	                    var totalEstimated   = 0;
-	                    var totalActual      = 0;
-	                    var totalExpenditure = 0;
-
-	                    angular.forEach(category.items, function(item) {
-	                        totalEstimated += item.estimated;
-	                        totalActual    += item.actual;
-	                    });
-	                    angular.forEach(category.expenditures, function(expenditure) {
-	                        totalExpenditure += expenditure.cost;
-	                    });
-	                    options.xAxis.categories.push(category.name);
-	                    options.series[0].data.push(totalEstimated);
-	                    options.series[1].data.push(totalActual);
-	                    options.series[2].data.push(totalExpenditure);
-	                });
-	            }
-	            barchart = new Highcharts.Chart(options);
+            if (categoryList.length == 0) {
+                options.xAxis.categories.push('No categories');
+                options.series[0].data.push(0.00);
+                options.series[1].data.push(0.00);
+                options.series[2].data.push(0.00);
+            } else {
+                angular.forEach(categoryList, function(category) {
+                    var totalEstimated   = 0;
+                    var totalActual      = 0;
+                    var totalExpenditure = 0;
+                    angular.forEach(category.items, function(item) {
+                        totalEstimated += item.estimated;
+                        totalActual    += item.actual;
+                    });
+                    angular.forEach(category.expenditures, function(expenditure) {
+                        totalExpenditure += expenditure.cost;
+                    });
+                    options.xAxis.categories.push(category.name);
+                    options.series[0].data.push(totalEstimated);
+                    options.series[1].data.push(totalActual);
+                    options.series[2].data.push(totalExpenditure);
+                });
             }
-            function error(response) {
-                vm.errorMsgGet = true;
-            }
+            $('#barchart-container').highcharts(options);
+            return response;
         }
 
-        function displayTable() {
-            return categoryService.retrieveList()
-                .then(getSuccess)
-                .catch(error);
+        function updateTable(response) {
+            vm.categoryList = response.data.objects;
+            var grandTotalEstimated   = 0;
+            var grandTotalActual      = 0;
+            var grandTotalExpenditure = 0;
 
-            function getSuccess(response) {
-                vm.categoryList = response.data.objects;
+            angular.forEach(vm.categoryList, function(category) {
+                var totalEstimated   = 0;
+                var totalActual      = 0;
+                var totalExpenditure = 0;
+                angular.forEach(category.items, function(item) {
+                    totalEstimated += item.estimated;
+                    totalActual    += item.actual;
+                });
+                angular.forEach(category.expenditures, function(expenditure) {
+                    totalExpenditure += expenditure.cost;
+                });
+                category.total_estimated   = totalEstimated;
+                category.total_actual      = totalActual;
+                category.total_expenditure = totalExpenditure;
+                grandTotalEstimated   += totalEstimated;
+                grandTotalActual      += totalActual;
+                grandTotalExpenditure += totalExpenditure;
 
-                var grandTotalEstimated   = 0;
-                var grandTotalActual      = 0;
-                var grandTotalExpenditure = 0;
-
-	            angular.forEach(response.data.objects, function(category) {
-	                var totalEstimated   = 0;
-	                var totalActual      = 0;
-	                var totalExpenditure = 0;
-	                angular.forEach(category.items, function(item) {
-	                    totalEstimated += item.estimated;
-	                    totalActual    += item.actual;
-	                });
-	                angular.forEach(category.expenditures, function(expenditure) {
-	                    totalExpenditure += expenditure.cost;
-	                });
-                    category.total_estimated   = totalEstimated;
-                    category.total_actual      = totalActual;
-                    category.total_expenditure = totalExpenditure;
-                    grandTotalEstimated   += totalEstimated;
-                    grandTotalActual      += totalActual;
-                    grandTotalExpenditure += totalExpenditure;
-
-	                if (totalExpenditure >= totalActual) {
-	                    category.paid   = 100;
-	                    category.unpaid = 0;
-	                } else {
-	                    category.paid   = Math.round(totalExpenditure / totalActual * 100);
-	                    category.unpaid = totalActual - totalExpenditure;
-	                }
-	            });
-	            $scope.grandTotalEstimated   = grandTotalEstimated;
-	            $scope.grandTotalActual      = grandTotalActual;
-	            $scope.grandTotalExpenditure = grandTotalExpenditure;
-            }
-            function error(response) {
-                vm.errorMsgGet = true;
-            }
+                if (totalExpenditure >= totalActual) {
+                    category.paid   = 100;
+                    category.unpaid = 0;
+                } else {
+                    category.paid   = Math.round(totalExpenditure / totalActual * 100);
+                    category.unpaid = totalActual - totalExpenditure;
+                }
+            });
+            vm.grandTotalEstimated   = grandTotalEstimated;
+            vm.grandTotalActual      = grandTotalActual;
+            vm.grandTotalExpenditure = grandTotalExpenditure;
+        }
+        
+        function error(response) {
+            vm.errorMsgGet = true;
         }
     }
 })();
