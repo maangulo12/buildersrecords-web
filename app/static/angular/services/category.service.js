bb4d44c612c7e23b90010056badfea792c97d3be
Miguel Angulo <maangulo12@gmail.com>
2016-01-17 18:18:02 -0600

    Update category service

--- app/static/angular/services/category.service.js
+++ app/static/angular/services/category.service.js
@@ -10,12 +10,35 @@
     function categoryService($http, store) {
         var url = store.get('api_url') + '/api/categories';
         var service = {
-            retrieveList: retrieveList
+            retrieveList: retrieveList,
+            create:       create,
+            update:       update,
+            remove:       remove
         };
         return service;
 
         function retrieveList() {
 			return $http.get(url + query('project_id', 'equals', store.get('project').id));
+        }
+
+        function create(name) {
+            var data = {
+                name:       name,
+                project_id: store.get('project').id
+            };
+            return $http.post(url, data);
+        }
+
+        function update(name) {
+            var data = {
+                name:       name,
+                project_id: store.get('project').id
+            };
+            return $http.put(url + '/' + store.get('category').id, data);
+        }
+
+        function remove() {
+            return $http.delete(url + '/' + store.get('category').id);
         }
     }
 
