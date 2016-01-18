5d99c9516b6e450ac1c82fbbd58da055e9e671e1
Miguel Angulo <maangulo12@gmail.com>
2016-01-18 09:52:03 -0600

    Update expenditure service

--- app/static/angular/services/expenditure.service.js
+++ app/static/angular/services/expenditure.service.js
@@ -10,10 +10,15 @@
     function expenditureService($http, store) {
         var url = store.get('api_url') + '/api/expenditures';
         var service = {
+            retrieveList:       retrieveList,
             retrieveByCategory: retrieveByCategory,
             removeByCategory:   removeByCategory
         };
         return service;
+
+        function retrieveList() {
+            return $http.get(url + query('project_id', 'equals', store.get('project').id));
+        }
 
         function retrieveByCategory() {
 			return $http.get(url + query('category_id', 'equals', store.get('category').id));
