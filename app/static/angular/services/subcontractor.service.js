90d2ae8790771657f75c9dbe21391fedfa9a7ebd
Miguel Angulo <maangulo12@gmail.com>
2016-01-19 22:32:47 -0600

    Update subcontractor service

--- app/static/angular/services/subcontractor.service.js
+++ app/static/angular/services/subcontractor.service.js
@@ -10,12 +10,45 @@
     function subcontractorService($http, store) {
         var url = store.get('api_url') + '/api/subcontractors';
         var service = {
-            retrieveList: retrieveList
+            retrieveList: retrieveList,
+            create:       create,
+            update:       update,
+            remove:       remove
         };
         return service;
 
         function retrieveList() {
             return $http.get(url + query('project_id', 'equals', store.get('project').id))
+                .then(successHandler)
+                .catch(errorHandler);
+        }
+
+        function create(vm) {
+            var data = {
+                name:           vm.name,
+                company:        vm.company,
+                contact_number: vm.contactNumber,
+                project_id:     store.get('project').id
+            };
+            return $http.post(url, data)
+                .then(successHandler)
+                .catch(errorHandler);
+        }
+
+        function update(vm) {
+            var data = {
+                name:           vm.name,
+                company:        vm.company,
+                contact_number: vm.contactNumber,
+                project_id:     store.get('project').id
+            };
+            return $http.put(url, data)
+                .then(successHandler)
+                .catch(errorHandler);
+        }
+
+        function remove(subcontractorId) {
+            return $http.delete(url + '/' + subcontractorId)
                 .then(successHandler)
                 .catch(errorHandler);
         }
