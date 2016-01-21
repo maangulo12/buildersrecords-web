ee25cf926e3a29630b97cfc263c645bb37c1ed74
Miguel Angulo <maangulo12@gmail.com>
2016-01-21 15:22:15 -0600

    Update projects controller

--- app/static/angular/modules/projects/projects.controller.js
+++ app/static/angular/modules/projects/projects.controller.js
@@ -22,6 +22,7 @@
             }
             function success(response) {
                 vm.projectList = response.data.objects;
+                return vm.projectList;
             }
             function error(response) {
                 vm.getError = true;
