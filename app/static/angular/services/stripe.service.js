d5294f69142c1d8eccfcd3b3283dcf64d1bb19ee
Miguel Angulo <maangulo12@gmail.com>
2016-01-21 15:15:58 -0600

    Update stripe service

--- app/static/angular/services/stripe.service.js
+++ app/static/angular/services/stripe.service.js
@@ -18,10 +18,10 @@
         };
         return service;
 
-        function validateCard(vm) {
-            var num = Stripe.card.validateCardNumber(vm.cardNumber);
-            var exp = Stripe.card.validateExpiry(vm.expMonth, vm.expYear);
-            var cvc = Stripe.card.validateCVC(vm.cvc);
+        function validateCard(card) {
+            var num = Stripe.card.validateCardNumber(card.cardNumber);
+            var exp = Stripe.card.validateExpiry(card.expMonth, card.expYear);
+            var cvc = Stripe.card.validateCVC(card.cvc);
 
             if (num && exp && cvc) {
                 return true;
@@ -31,13 +31,13 @@
             }
         }
 
-        function createCardToken(vm) {
+        function createCardToken(card) {
             var data = {
-                number:    vm.cardNumber,
-                cvc:       vm.cvc,
-                exp_month: vm.expMonth,
-                exp_year:  vm.expYear,
-                name:      vm.cardName.toUpperCase()
+                number:    card.cardNumber,
+                cvc:       card.cvc,
+                exp_month: card.expMonth,
+                exp_year:  card.expYear,
+                name:      card.cardName.toUpperCase()
             };
             return $q(function(resolve, reject) {
                 Stripe.card.createToken(data, responseCallback);
@@ -52,12 +52,12 @@
             });
         }
 
-        function createSubscription(vm, tokenId) {
+        function createSubscription(user, tokenId) {
             var data = {
-                email:    vm.email,
-                username: vm.username,
-                password: vm.password,
-                plan:     vm.plan,
+                email:    user.email,
+                username: user.username,
+                password: user.password,
+                plan:     user.plan,
                 token_id: tokenId
             };
             return $http.post(url, data)
