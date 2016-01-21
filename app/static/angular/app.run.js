a1d29f1b21d12317a209fa043b2c3f2d612d495f
Miguel Angulo <maangulo12@gmail.com>
2016-01-20 22:40:10 -0600

    Update app run

--- app/static/angular/app.run.js
+++ app/static/angular/app.run.js
@@ -9,10 +9,9 @@
 
     function runBlock($rootScope, $state, store, jwtHelper) {
         // CHANGE THESE FOR PRODUCTION DEPLOYMENT
-        store.set('api_url', 'https://buildersrecords-api-staging.herokuapp.com');
+        store.set('url', 'https://buildersrecords-api-staging.herokuapp.com');
         Stripe.setPublishableKey('pk_test_KY3H8e295UxwoHrrqHBobKRC');
         // Restricts access to routes that require login
-        // Checks if the token is expired
         $rootScope.$on('$stateChangeStart', change);
         function change(e, to) {
             if (to.data && to.data.requiresLogin) {
