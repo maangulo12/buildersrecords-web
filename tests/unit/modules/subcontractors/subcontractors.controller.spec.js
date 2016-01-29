ba4127ebe078987be701f4b88337895a9075b274
Miguel Angulo <maangulo12@gmail.com>
2016-01-29 12:42:52 -0600

    Add test stories

---
+++ tests/unit/modules/subcontractors/subcontractors.controller.spec.js
@@ -0,0 +1,27 @@
+(function() {
+    'use strict';
+
+    var assert = require('chai').assert;
+
+    describe('SubcontractorsController Test', function() {
+        it('should get Subcontractors', function() {
+            // TODO
+        });
+
+        it('should add Subcontractor', function() {
+            // TODO
+        });
+
+        it('should delete many Subcontractors', function() {
+            // TODO
+        });
+
+        it('should delete Subcontractor', function() {
+            // TODO
+        });
+
+        it('should update Subcontractor', function() {
+            // TODO
+        });
+    });
+})();
