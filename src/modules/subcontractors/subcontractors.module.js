5c673915eeed5c5f3c1632b058c048003124c184
Miguel Angulo <maangulo12@gmail.com>
2016-01-29 12:42:41 -0600

    Update subcontractors module

--- src/modules/subcontractors/subcontractors.module.js
+++ src/modules/subcontractors/subcontractors.module.js
@@ -1,5 +1,13 @@
 (function() {
     'use strict';
 
-    angular.module('app.projects.subcontractors', []);
+    angular.module('app.projects.subcontractors', [
+        /* Vendor modules */
+        'ngMessages',
+        'ui.router',
+        'angular-storage',
+        'smart-table',
+        /* App services */
+        'app.service.subcontractor'
+    ]);
 })();
