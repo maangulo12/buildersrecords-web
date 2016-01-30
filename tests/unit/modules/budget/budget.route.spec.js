404bfa9119e322f3a8dc353e9a9641c521571d37
Miguel Angulo <maangulo12@gmail.com>
2016-01-30 11:14:46 -0600

    Add unit tests

---
+++ tests/unit/modules/budget/budget.route.spec.js
@@ -0,0 +1,19 @@
+(function() {
+    'use strict';
+
+    var assert = require('chai').assert;
+
+    describe('Budget Route Test', function() {
+        it('should route to /projects/budget', function() {
+            // TODO
+        });
+
+        it('should get User', function() {
+            // TODO
+        });
+
+        it('should get NavController', function() {
+            // TODO
+        });
+    });
+})();
