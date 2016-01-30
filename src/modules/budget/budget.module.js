c733757639d13198b9a37c2386062448150fa9ea
Miguel Angulo <maangulo12@gmail.com>
2016-01-30 11:14:16 -0600

    Update budget module dependencies

--- src/modules/budget/budget.module.js
+++ src/modules/budget/budget.module.js
@@ -1,5 +1,17 @@
 (function() {
     'use strict';
 
-    angular.module('app.projects.budget', []);
+    angular.module('app.projects.budget', [
+        /* Vendor modules */
+        'ngMessages',
+        'ui.router',
+        'angular-storage',
+        'smart-table',
+        /* App directives */
+        'app.directive.format',
+        /* App services */
+        'app.service.category',
+        'app.service.expenditure',
+        'app.service.item'
+    ]);
 })();
