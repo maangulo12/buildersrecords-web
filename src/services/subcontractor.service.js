9c78f27d0d5480277760c53c3c3ab6a5d42cfbbb
Miguel Angulo <maangulo12@gmail.com>
2016-01-30 11:14:33 -0600

    Update subcontractor service

--- src/services/subcontractor.service.js
+++ src/services/subcontractor.service.js
@@ -1,3 +1,7 @@
+/**
+ * Subcontractor Service
+ * @namespace Services
+ */
 (function() {
     'use strict';
 
@@ -7,6 +11,11 @@
 
     subcontractorService.$inject = ['$http', 'store', '$q'];
 
+    /**
+     * @namespace Subcontractor Service
+     * @desc functions for Subcontractor Service
+     * @memberOf Services
+     */
     function subcontractorService($http, store, $q) {
         var url = store.get('url') + '/api/subcontractors';
         var service = {
@@ -17,6 +26,12 @@
         };
         return service;
 
+        /**
+         * @name retrieveList
+         * @desc Retrieves a list of Subcontractors
+         * @returns {Promise}
+         * @memberOf Services.subcontractorService
+         */
         function retrieveList() {
             return $http.get(url + query('project_id', 'equals', store.get('project').id))
                 .then(success)
