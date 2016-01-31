2459199d62dfd0de785bd31b7e5e0d7724f7f27f
Miguel Angulo <maangulo12@gmail.com>
2016-01-30 18:49:37 -0600

    Update subcontractor service

--- src/services/subcontractor.service.js
+++ src/services/subcontractor.service.js
@@ -1,7 +1,10 @@
 /**
- * Subcontractor Service
- * @namespace Services
- */
+ * @ngdoc service
+ * @name subcontractorService
+ * @description
+ *
+ * This is the service module for Subcontractor.
+**/
 (function() {
     'use strict';
 
@@ -9,11 +12,6 @@
         .module('app.service.subcontractor', [])
         .factory('subcontractorService', subcontractorService);
 
-    /**
-     * @namespace Subcontractor Service
-     * @desc functions for Subcontractor Service
-     * @memberOf Services
-     */
     subcontractorService.$inject = ['$http', 'store', '$q'];
 
     function subcontractorService($http, store, $q) {
@@ -26,24 +24,12 @@
         };
         return service;
 
-        /**
-         * @name retrieveList
-         * @desc Retrieves a list of Subcontractors
-         * @returns {Promise}
-         * @memberOf Services.subcontractorService
-         */
         function retrieveList() {
             return $http.get(url + query('project_id', 'equals', store.get('project').id))
                 .then(success)
                 .catch(error);
         }
 
-        /**
-         * @name create
-         * @desc creates a Subcontractor
-         * @returns {Promise}
-         * @memberOf subcontractorService
-         */
         function create(subcontractor) {
             var data = {
                 name:           subcontractor.name,
@@ -56,12 +42,6 @@
                 .catch(error);
         }
 
-        /**
-         * @name update
-         * @desc updates a Subcontractor
-         * @returns {Promise}
-         * @memberOf subcontractorService
-         */
         function update(subcontractor) {
             var data = {
                 name:           subcontractor.name,
@@ -74,12 +54,6 @@
                 .catch(error);
         }
 
-        /**
-         * @name remove
-         * @desc removes a Subcontractor
-         * @returns {Promise}
-         * @memberOf subcontractorService
-         */
         function remove(subcontractor) {
             return $http.delete(url + '/' + subcontractor.id)
                 .then(success)
