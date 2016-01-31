32fe88de3674b87fbaeeb3f889f1f1597c1da0d4
Miguel Angulo <maangulo12@gmail.com>
2016-01-30 18:48:58 -0600

    Update docs

---
+++ docs/js/docs-setup.js
@@ -0,0 +1,48 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "subcontractorService",
+      "shortName": "subcontractorService",
+      "type": "service",
+      "moduleName": "subcontractorService",
+      "shortDescription": "This is the service module for Subcontractor.",
+      "keywords": "api module service subcontractor subcontractorservice"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "__file": "_FAKE_DEST_/js/docs-setup.js",
+  "__options": {
+    "startPage": "/api",
+    "scripts": [
+      "js/angular.min.js",
+      "js/angular-animate.min.js",
+      "js/marked.js"
+    ],
+    "styles": [],
+    "title": "API Documentation",
+    "html5Mode": true,
+    "editExample": true,
+    "navTemplate": false,
+    "navContent": "",
+    "navTemplateData": {},
+    "loadDefaults": {
+      "angular": true,
+      "angularAnimate": true,
+      "marked": true
+    }
+  },
+  "html5Mode": true,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "js/angular.min.js",
+    "js/angular-animate.min.js",
+    "js/marked.js"
+  ]
+};
