a3c039fe14ab400c556de34568c72f6c75c31269
Miguel Angulo <maangulo12@gmail.com>
2016-02-01 10:10:20 -0600

    Add expenditures tests

---
+++ tests/unit/modules/expeditures/expenditures.controller.spec.js
@@ -0,0 +1,27 @@
+(function() {
+    'use strict';
+
+    var assert = require('chai').assert;
+
+    describe('ExpendituresController Test', function() {
+        it('should get Expenditure List', function() {
+            // TODO
+        });
+
+        it('should add Expenditure', function() {
+            // TODO
+        });
+
+        it('should delete many Expenditures', function() {
+            // TODO
+        });
+
+        it('should delete Expenditure', function() {
+            // TODO
+        });
+
+        it('should update Expenditure', function() {
+            // TODO
+        });
+    });
+})();
