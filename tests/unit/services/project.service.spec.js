d92ae03e6f23f832adbb40f3703d6b76c2721ea1
Miguel A. Angulo <maangulo12@gmail.com>
2016-02-01 19:20:52 -0600

    Add project service test

---
+++ tests/unit/services/project.service.spec.js
@@ -0,0 +1,23 @@
+(function() {
+    'use strict';
+
+    var assert = require('chai').assert;
+
+    describe('Project Service Test', function() {
+        it('should get Project List', function() {
+            // TODO
+        });
+
+        it('should add Project', function() {
+            // TODO
+        });
+
+        it('should update Project', function() {
+            // TODO
+        });
+
+        it('should delete Project', function() {
+            // TODO
+        });
+    });
+})();
