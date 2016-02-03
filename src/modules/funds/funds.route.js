ed24f16c8e90cb4b0ac58981eb4105bc22362c85
Miguel Angulo <maangulo12@gmail.com>
2016-02-02 22:31:12 -0600

    Update funds module

--- src/modules/funds/funds.route.js
+++ src/modules/funds/funds.route.js
@@ -11,25 +11,13 @@
         $stateProvider.state('funds', {
             url: '/projects/funds',
             resolve: {
-                User: function(userService, $q) {
-                    return userService.retrieve()
-                        .then(responseHandler)
-                        .catch(errorHandler);
-
-                    function responseHandler(response) {
-                        return response.data;
-                    }
-                    function errorHandler(response) {
-                        return $q.reject(response.data);
-                    }
-                }
+                User: updateUser
             },
             views: {
                 'nav': {
                     templateUrl: 'www/html/nav2.html',
-                    controller: function($scope, User) {
-                        $scope.username = User.username;
-                    }
+                    controller:   NavController,
+                    controllerAs: 'vm'
                 },
                 'body': {
                     templateUrl:  'www/html/funds.html',
@@ -41,5 +29,30 @@
                 requiresLogin: true
             }
         });
+    }
+
+    updateUser.$inject = ['userService', '$q'];
+
+    function updateUser(userService, $q) {
+        return getUser()
+            .then(success)
+            .catch(error);
+
+        function getUser() {
+            return userService.retrieve();
+        }
+        function success(response) {
+            return $q.resolve(response.data);
+        }
+        function error(response) {
+            return $q.reject(response);
+        }
+    }
+
+    NavController.$inject = ['User'];
+
+    function NavController(User) {
+        var vm = this;
+        vm.username = User.username;
     }
 })();
