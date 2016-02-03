dda07dd5c13f5aef8a7ac470eaa52c323d5206dd
Miguel Angulo <maangulo12@gmail.com>
2016-02-02 22:31:23 -0600

    Add draw service tests

---
+++ tests/unit/services/draw.service.spec.js
@@ -0,0 +1,23 @@
+(function() {
+    'use strict';
+
+    var assert = require('chai').assert;
+
+    describe('Draw Service Test', function() {
+        it('should get Draws List', function() {
+            // TODO
+        });
+
+        it('should add Draw', function() {
+            // TODO
+        });
+
+        it('should update Draw', function() {
+            // TODO
+        });
+
+        it('should delete Draw', function() {
+            // TODO
+        });
+    });
+})();
