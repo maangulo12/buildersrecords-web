9f466ca1b8c6bde99fb7fc1b43a0aeb77fbc4948
Miguel Angulo <maangulo12@gmail.com>
2016-02-08 15:20:44 -0600

    Add sample test

--- src/modules/subcontractors/subcontractors.controller.spec.js
+++ src/modules/subcontractors/subcontractors.controller.spec.js
@@ -3,6 +3,16 @@
 
     var assert = require('chai').assert;
 
+    // Testing purposes
+    describe('Array', function(){
+        describe('#indexOf()', function(){
+            it('should return -1 when the value is not present', function(){
+                assert.equal(-1, [1,2,3].indexOf(5));
+                assert.equal(-1, [1,2,3].indexOf(0));
+            });
+        });
+    });
+
     describe('SubcontractorsController Test', function() {
         it('should get Subcontractors', function() {
             // TODO
