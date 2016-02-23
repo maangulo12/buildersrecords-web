6202551b44b101faa18918cc886f13c2accd5263
Miguel Angulo <maangulo12@gmail.com>
2016-02-23 12:48:46 -0600

    Comment publishable key

--- src/app.constants.js
+++ src/app.constants.js
@@ -3,6 +3,6 @@
 
     angular
         .module('app')
-        .constant('API_URL', 'https://buildersrecords-api-staging.herokuapp.com')
-        .constant('PUBLISHABLE_KEY', 'pk_test_KY3H8e295UxwoHrrqHBobKRC');
+        .constant('API_URL', 'https://buildersrecords-api-staging.herokuapp.com');
+        // .constant('PUBLISHABLE_KEY', 'pk_test_KY3H8e295UxwoHrrqHBobKRC');
 })();
