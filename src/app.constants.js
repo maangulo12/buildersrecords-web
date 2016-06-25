1ff8d34e1252482b2aa582c2b00b17f872c5f968
Miguel A. Angulo <maangulo12@gmail.com>
2016-06-24 23:15:07 -0500

    Remove stripe constant

--- src/app.constants.js
+++ src/app.constants.js
@@ -4,5 +4,4 @@
     angular
         .module('app')
         .constant('API_URL', 'https://buildersrecords-api-staging.herokuapp.com');
-        // .constant('PUBLISHABLE_KEY', 'pk_test_KY3H8e295UxwoHrrqHBobKRC');
 })();
