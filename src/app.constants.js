7e5db336d8dc1ad1c52e8f02810d4ec52e52eb4c
Miguel Angulo <maangulo12@gmail.com>
2016-07-02 14:21:45 -0500

    Add app constant

--- src/app.constants.js
+++ src/app.constants.js
@@ -4,4 +4,5 @@
   angular
     .module('app')
     .constant('API_URL', 'https://buildersrecords-api-staging.herokuapp.com');
+    // .constant('API_URL', 'https://buildersrecords-api.herokuapp.com');
 })();
