ab6c953fc528ffefdef49453cf5fac991a3293c1
Miguel Angulo <maangulo12@gmail.com>
2016-07-07 14:00:02 -0500

    folder src -> app

--- src/app.constants.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('app')
-    .constant('API_URL', 'https://buildersrecords-api-staging.herokuapp.com');
-    // .constant('API_URL', 'https://buildersrecords-api.herokuapp.com');
-})();
